9db7a839c22865de7b4a1f5470a8a58c26391035
courtneymickelsen <lam15020@byui.edu>
2022-12-20 13:20:55 -0700

    css changes, current vs all flavors

--- public/js/menu.js
+++ public/js/menu.js
@@ -1,9 +1,12 @@
-let menuCardsDiv = document.querySelector('.menu-cards-div');
-let menuCardsHtml = "";
+let currentMenuCardsDiv = document.querySelector('.current.menu-cards-div');
+let otherMenuCardsDiv = document.querySelector('.other.menu-cards-div');
+let currentMenuCardsHtml = "";
+let otherMenuCardsHtml = "";
 let databaseList = [
     {
         "title": "Huckleberry Cheesecake",
         "price": 2.00,
+        "current": true,
         "info": "Stuffed with huckleberry cream cheese filling.", 
         "topPath": "../imgs/huckleberry1.jpg",
         "sidePath": "../imgs/huckleberry4.jpg"
@@ -11,6 +14,7 @@ let databaseList = [
     {
         "title": "Lemon",
         "price": 2.00,
+        "current": false,
         "info": "Filled with creamy vanilla butter cream and a sweet tangy lemon curd center.",
         "topPath": "../imgs/lemon1.jpg",
         "sidePath": "../imgs/lemon2.jpg"
@@ -18,6 +22,7 @@ let databaseList = [
     {
         "title": "Orange Creamsicle",
         "price": 2.00,
+        "current": true,
         "info": "Filled with orange creamsicle flavored buttercream.",
         "topPath": "../imgs/orange1.jpg",
         "sidePath": "../imgs/orange2.jpg"
@@ -25,6 +30,7 @@ let databaseList = [
     {
         "title": "Strawberry",
         "price": 2.00,
+        "current": false,
         "info": "Filled with strawberry buttercream with sugar sprinkles on the sides.",
         "topPath": "../imgs/strawberry3.jpg",
         "sidePath": "../imgs/strawberry2.jpg"
@@ -32,6 +38,7 @@ let databaseList = [
     {
         "title": "Swanky Dip",
         "price": 2.50,
+        "current": false,
         "info": "Chocolate ganache dipped in lush dark chocolate.",
         "topPath": "../imgs/swankydip3.jpg",
         "sidePath": "../imgs/swankydip4.jpg"
@@ -39,6 +46,7 @@ let databaseList = [
     {
         "title": "Mango",
         "price": 2.50,
+        "current": false,
         "info": "Chocolate ganache dipped in lush dark chocolate.",
         "topPath": "../imgs/mango1.jpg",
         "sidePath": "../imgs/mangostrawberry4.jpg"
@@ -46,6 +54,7 @@ let databaseList = [
     {
         "title": "Cotton Candy",
         "price": 2.00,
+        "current": false,
         "info": "Filled with cotton candy flavored buttercream.",
         "topPath": "../imgs/cottoncandy1.jpg",
         "sidePath": "../imgs/cottoncandy2.jpg"
@@ -53,19 +62,26 @@ let databaseList = [
 ]
 
 databaseList.forEach(item => {
-    menuCardsHtml += `
-        <div class='menu-card'>
-            <img src='${item.topPath}' class='menu-card-img'>
-            <img src='${item.sidePath}' class='menu-card-hover-img'>
-            <h3>${item.title} | $${item.price}</h3>
-            <p>${item.info}</p>
-        </div>
-    `;
+    if (item.current) {
+        currentMenuCardsHtml += `
+            <div class='menu-card'>
+                <img src='${item.topPath}' class='menu-card-img'>
+                <img src='${item.sidePath}' class='menu-card-hover-img'>
+                <h3>${item.title} | $${item.price}</h3>
+                <p>${item.info}</p>
+            </div>
+        `;
+    } else {
+        otherMenuCardsHtml += `
+            <div class='menu-card'>
+                <img src='${item.topPath}' class='menu-card-img'>
+                <img src='${item.sidePath}' class='menu-card-hover-img'>
+                <h3>${item.title} | $${item.price}</h3>
+                <p>${item.info}</p>
+            </div>
+        `;
+    }
 });
 
-menuCardsDiv.innerHTML = menuCardsHtml;
-
-// let menuCardImageList = document.querySelectorAll('menu-card-img');
-// menuCardImageList.forEach(item => {
-//     item.addEventListener('on');
-// })
+currentMenuCardsDiv.innerHTML = currentMenuCardsHtml;
+otherMenuCardsDiv.innerHTML = otherMenuCardsHtml;
